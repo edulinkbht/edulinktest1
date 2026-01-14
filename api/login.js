5409bbcd0fb598be5ba5b309af6003e1d612dc58
phuentx <phuentx@gmail.com>
2026-01-14 21:41:34 +0600

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,6 +1,5 @@
 export default function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).end();
-
   res.status(200).json({
     apiKey: "AIzaSyAJH79UQl0rc96Qug0DKLevO4ZI_sn8Kno",
     authDomain: "edulinki.firebaseapp.com",
